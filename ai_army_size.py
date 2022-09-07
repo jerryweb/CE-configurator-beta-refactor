5f1de3363a54e34876c6e0d4a91426ab0f13b5af
Jerry Webb <ljwebbdev8800@gmail.com>
2022-09-06 20:52:26 -0700

    Fixed Bot Resource bug and Difficulty files not working

--- ai_army_size.py
+++ ai_army_size.py
@@ -9,39 +9,60 @@ def setAiArmySizeModule(rootWindow, fileToRead):
     aiSizeWindow.title("AI Army Size")
     aiSizeWindow.geometry('700x150')
     aiSizeCurrent = ""
+    print(fileToRead)
+    # with open(fileToRead, "r") as lengthFile:
+    #     wholeFile = lengthFile.read()
 
-    with open(fileToRead, "r") as lengthFile:
-        wholeFile = lengthFile.read()
+        # currentLength2 = r"BotResources +\d+"
+        # currentLength3 = str(re.search(currentLength2, wholeFile).group())
+        # currentLength4 = currentLength3.strip("[']")
+        # currentLength5 = currentLength4.replace("BotResources", "").strip()
 
-        currentLength2 = r"BotResources +\d+"
-        currentLength3 = str(re.search(currentLength2, wholeFile).group())
-        currentLength4 = currentLength3.strip("[']")
-        currentLength5 = currentLength4.replace("BotResources", "").strip()
+
+    lengthFile = open(fileToRead, "r")
+    wholeFile = lengthFile.read()
+    # currentAISizeLine = r"BotResources +\d+"
+    # currentAISizeLine = str(re.search(currentAISizeLine, wholeFile).group())
+    currentAISizeLine = linecache.getline(r"" + fileToRead, 37)
+    oldAiSizeLine = currentAISizeLine
+    currentAISizeLine = currentAISizeLine.strip()
+    currentAISizeLine = currentAISizeLine.strip("{BotResources}")
+    currentAISizeLine = currentAISizeLine.strip()
+    linecache.clearcache()
+    print(currentAISizeLine)
 
     def saveChanges():
-        with open(fileToRead, "w") as lengthFile:
-            # Check that user input is an integer (number)
-            try:
-                currentPoints.get().isdigit()
-                newPoints = float(currentPoints.get())
-                lengthFile.write(wholeFile.replace(currentLength4, str(f"BotResources {newPoints}")))
-                messagebox.showinfo("Saved", "Values Saved")
-                aiSizeWindow.destroy()
-            # If not reset it back to the default value
-            except ValueError:
-                messagebox.showerror("Error", "Error, please enter a decimal number")
-                currentPoints.delete(0, END)
-                currentPoints.insert(0, currentLength5)
-                aiSizeWindow.focus_force()
+        # with open(fileToRead, "w") as lengthFile:
+        # Check that user input is an integer (number)
+        try:
+            fileToWrite = open(fileToRead, "w")
+            currentPoints.get().isdigit()
+            print(currentPoints.get())
+            # newPoints = float(currentPoints.get())
+            # lengthFile.write(wholeFile.replace(currentLength4, str(f"BotResources {newPoints}")))
+            fileToWrite.write(wholeFile.replace(oldAiSizeLine, "			{BotResources " + str(float(currentPoints.get())) + "}\n"))
+            messagebox.showinfo("Saved", "Values Saved")
+            aiSizeWindow.destroy()
+        # If not reset it back to the default value
+        except ValueError:
+            messagebox.showerror("Error", "Error, please enter a decimal number")
+            currentPoints.delete(0, END)
+            # currentPoints.insert(0, currentLength5)
+            currentPoints.insert(0, currentAISizeLine)
+            aiSizeWindow.focus_force()
 
     currentPoints = Entry(aiSizeWindow, textvariable=aiSizeCurrent, width=15)
     currentPoints.delete(0, END)
-    currentPoints.insert(0, currentLength5)
+    # currentPoints.insert(0, currentLength5)
+    currentPoints.insert(0, currentAISizeLine)
     currentPoints.grid(row=2, column=2)
 
     saveButton = Button(aiSizeWindow, text="Save changes", command=saveChanges)
     saveButton.grid(row=3, column=2, padx=3)
 
+    # aiSizeLabel = Label(aiSizeWindow,
+    #                      text=f"Current AI army size multiplier is {currentLength5}. Normally it's between 6 and 7").grid(
+    #     row=1, column=2)
     aiSizeLabel = Label(aiSizeWindow,
-                         text=f"Current AI army size multiplier is {currentLength5}. Normally it's between 6 and 7").grid(
+                         text=f"Current AI army size multiplier is {currentAISizeLine}. Normally it's between 6 and 7").grid(
         row=1, column=2)
--- main.py
+++ main.py
@@ -18,16 +18,25 @@ root.geometry("800x600")
 
 # defined variables
 difficulty = StringVar(value="normal")
+fileToRead = getDifficultyFileToRead(difficulty=difficulty)
 economySectionRow = 1
-
 
 selectDifficultyLabel = Label(root, text=selectDifficultyText)
 selectDifficultyLabel.grid(row=1, column=3)
 
-performanceRadiobutton = Radiobutton(root, text=performanceText, variable=difficulty, value="performance").grid(row=2,
-                                                                                                                column=3)
+
+# Get the file to read based on which difficulty is selected
+def getDifficultyFile():
+   j = getDifficultyFileToRead(difficulty=difficulty)
+
+
+
+performanceRadiobutton = Radiobutton(root, text=performanceText, variable=difficulty, value="performance")
+performanceRadiobutton.grid(row=2, column=3)
+
 normalRadiobutton = Radiobutton(root, text=normalText, variable=difficulty, value="normal").grid(row=3, column=3)
-hardRadiobutton = Radiobutton(root, text=hardText, variable=difficulty, value="hard").grid(row=4, column=3)
+hardRadiobutton = Radiobutton(root, text=hardText, variable=difficulty, value="hard").grid(
+    row=4, column=3)
 unfairRadiobutton = Radiobutton(root, text=unfairText, variable=difficulty, value="unfair").grid(row=5, column=3)
 
 spacer = Label(root, text=spacerSize32)
@@ -37,35 +46,36 @@ economyLabel = Label(root, text=economyText)
 economyLabel.grid(row=economySectionRow, column=1)
 economySectionRow += 1
 
-# Get the file to read based on which difficulty is selected
-fileToRead = getDifficultyFileToRead(difficulty=difficulty)
-
 # BUTTONS
 
 aiArmyButton = Button(root, text="AI army size",
-                      command=lambda: setAiArmySizeModule(rootWindow=root, fileToRead=fileToRead))
+                      command=lambda: setAiArmySizeModule(rootWindow=root, fileToRead=getDifficultyFileToRead(
+                                                                             difficulty=difficulty)))
 aiArmyButton.grid(row=economySectionRow, column=1)
 economySectionRow += 1
 
 playerArmyButton = Button(root, text="Player army size",
-                          command=lambda: setPlayerArmySizeModule(rootWindow=root, fileToRead=fileToRead))
+                          command=lambda: setPlayerArmySizeModule(rootWindow=root, fileToRead=getDifficultyFileToRead(
+                                                                             difficulty=difficulty)))
 playerArmyButton.grid(row=economySectionRow, column=1)
 economySectionRow += 1
 
 resourcesStartButton = Button(root, text="Starting Player Resources",
-                              command=lambda: setStartingPlayerResources(rootWindow=root, fileToRead=fileToRead))
+                              command=lambda: setStartingPlayerResources(rootWindow=root,
+                                                                         fileToRead=getDifficultyFileToRead(
+                                                                             difficulty=difficulty)))
 resourcesStartButton.grid(row=economySectionRow, column=1)
 economySectionRow += 1
 
 resourcesButton = Button(root, text="Resource Income",
-                         command=lambda: resourceIncome(rootWindow=root, fileToRead=fileToRead))
+                         command=lambda: resourceIncome(rootWindow=root,
+                                                        fileToRead=getDifficultyFileToRead(difficulty=difficulty)))
 resourcesButton.grid(row=economySectionRow, column=1)
 economySectionRow += 1
 
 spacer = Label(root, text=spacerSize32)
 spacer.grid(row=economySectionRow, column=1)
 economySectionRow += 1
-
 
 gameMechanicsRow = economySectionRow
 
@@ -98,31 +108,41 @@ spacer = Label(root, text=spacerSize32)
 spacer.grid(row=gameMechanicsRow, column=1)
 gameMechanicsRow += 1
 
-
 aiLogicRow = gameMechanicsRow
 
 gameMechanicsLabel = Label(root, text=aiLogicText)
 gameMechanicsLabel.grid(row=aiLogicRow, column=1)
 aiLogicRow += 1
 
-aiResearchButton = Button(root, text="AI Research Speed Progression", command=lambda: aiResearches(rootWindow=root,fileToRead=fileToRead))
-aiResearchButton.grid(row=aiLogicRow,column=1)
+aiResearchButton = Button(root, text="AI Research Speed Progression", command=lambda: aiResearches(rootWindow=root,
+                                                                                                   fileToRead=getDifficultyFileToRead(
+                                                                                                       difficulty=difficulty)))
+aiResearchButton.grid(row=aiLogicRow, column=1)
 aiLogicRow += 1
 
-aiDefenseResearchSpeedButton = Button(root, text="AI defense stage research speed", command=lambda: aiDefenseResearchSpeed(rootWindow=root,fileToRead=fileToRead))
-aiDefenseResearchSpeedButton.grid(row=aiLogicRow,column=1)
+aiDefenseResearchSpeedButton = Button(root, text="AI defense stage research speed",
+                                      command=lambda: aiDefenseResearchSpeed(rootWindow=root,
+                                                                             fileToRead=getDifficultyFileToRead(
+                                                                                 difficulty=difficulty)))
+aiDefenseResearchSpeedButton.grid(row=aiLogicRow, column=1)
 aiLogicRow += 1
 
-aiReinforcementTimeButton = Button(root, text="Defense Preparation Time (AI reinforcement Time)", command=lambda: preparationTime(rootWindow=root))
-aiReinforcementTimeButton.grid(row=aiLogicRow,column=1)
+aiReinforcementTimeButton = Button(root, text="Defense Preparation Time (AI reinforcement Time)",
+                                   command=lambda: preparationTime(rootWindow=root))
+aiReinforcementTimeButton.grid(row=aiLogicRow, column=1)
 aiLogicRow += 1
+
 
 def showhelp():
     helpWindow = Toplevel(root)
-    textLabel = Label(helpWindow,text="This programme allows you to easily edit mod files. \n But the guide linked in the mod description \n is still the safest way to edit the game.\n If you have any questions about this programme please message me \n in discord linked in the description or in official GoH discord.").grid(row=5,column=2)
+    textLabel = Label(helpWindow,
+                      text="This programme allows you to easily edit mod files. \n But the guide linked in the mod description \n is still the safest way to edit the game.\n If you have any questions about this programme please message me \n in discord linked in the description or in official GoH discord.").grid(
+        row=5, column=2)
 
-Button(root, text="help", command=showhelp).grid(row=10,column=3)
-madeByLabel =Label(root, text="Made by MrCookie/ refactored by Hawka for Conquest Enhanced mod.\n The code will be available on github if you want to do\n use it for your mod.").grid (row=11,column=3)
 
+Button(root, text="help", command=showhelp).grid(row=10, column=3)
+madeByLabel = Label(root,
+                    text="Made by MrCookie/ refactored by Hawka for Conquest Enhanced mod.\n The code will be available on github if you want to do\n use it for your mod.").grid(
+    row=11, column=3)
 
 root.mainloop()
